4ef829da1e4f4520bfcdcb888305c39327392741
Noe Zavaleta C <nzavaletac@gmail.com>
2022-06-02 23:22:07 -0500

    register and login is finished and generate token session

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,49 @@
-import { validationResult } from "express-validator";
+import { User } from "../models/User.models.js";
+import jwt from "jsonwebtoken";
 
-export const register = (req, res) => {
-  res.json({ ok: "Register"  });
+export const register = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // validacion alternativa 2 buscando por email
+    let user = await User.findOne({ email });
+    if (user) throw { code: 11000 };
+
+    user = new User({ email, password });
+    await user.save();
+    // Generar jwt token
+
+    return res.status(201).json({ user });
+  } catch (error) {
+    // validacion  alternativa 1 por defecti mongoose
+    console.log(error.code);
+    if (error.code === 11000) {
+      return res.status(400).json({ error: "Ya existe este usuario." });
+    }
+    return res.status(500).json({ error: "Error de servidor." });
+  }
 };
 
-export const login = (req, res) => {
-  res.json({ ok: "login" });
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (!user)
+      return res
+        .status(403)
+        .json({ error: "NO existe el usuario registrado." });
+    const respuestaPassword = await user.comparePassword(password);
+    if (!respuestaPassword) {
+      return res.status(403).json({ error: "Contraseña incorrecta." });
+    }
+    // Generar jwt token
+    const token = jwt.sign(
+      { uid: user._id, email: user.email },
+      process.env.JWT_SECRET
+    );
+
+    return res.json({ token: token, user });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Error de servidor." });
+  }
 };
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 import authRouter from "./routes/auth.route.js";
 const app = express();
 app.use(express.json());
-app.use("/api/v1", authRouter);
+app.use("/api/v1/auth", authRouter);
 
 const PORT = process.env.PORT || 5001;
 
